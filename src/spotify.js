4286a176938a98ecd754487ca6de767be977b60e
prakhar <prakhar623@gmail.com>
2021-07-15 19:54:54 +0530

    first commit

---
+++ src/spotify.js
@@ -0,0 +1,32 @@
+//giving real spotify power to check authentication
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "fbe2d1d624cb41c3b9819c6dbaa71d6f";
+//as soon as it login it will redirect to our own home page so we use redirect
+const redirectUri = "https://itslike-spotify.web.app/";
+//are the permissions that u r authorised to do
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state"
+];
+
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
